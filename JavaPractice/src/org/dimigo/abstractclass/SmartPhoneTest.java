01d58730d0dfb93046a4409c6780f2987eaa5e90
선우 <@sunwoo>
2015-09-03 11:13:39 +0900

    실습과제 3 2609 박선우

---
+++ JavaPractice/src/org/dimigo/abstractclass/SmartPhoneTest.java
@@ -0,0 +1,20 @@
+package org.dimigo.abstractclass;
+public class SmartPhoneTest {
+	public static void main(String[] args) {
+		SmartPhone[] phones = {
+				new iPhone("iphone", "애플", 700000),
+				new Galaxy("갤럭시 s6", "삼성", 650000)
+		};
+		
+		for(SmartPhone phone : phones) {
+			System.out.println(phone);
+			phone.turnOn();
+			phone.pay();
+			
+			phone.useSpecialFunction();
+			
+			phone.turnOff();
+			System.out.println();
+		}
+	}
+}
